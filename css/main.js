7b61a47a0b02b27a4280dd0afd756e5d6c909dc0
LitvenkoAA <146692051+LitvenkoAA@users.noreply.github.com>
2023-11-20 14:35:53 +0500

    Update main.js

--- css/main.js
+++ css/main.js
@@ -1,4 +1,4 @@
-(function () {
+/*(function () {
     const header = document.querySelector('.header');
     window.onscroll=()=>{
         if (window.pageYOffset > 50) {
@@ -7,4 +7,16 @@
             header.classList.remove('header_active')
         }
     };
-}())
+}())*/
+
+/*Смена тона header при скроле с помощью addEvLi*/
+
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".header");
+    if (window.pageYOffset > 100) {
+        header.classList.add("header_active");
+    } else {
+        header.classList.remove("header_active");
+    }
+    });
